866490c35ddf8dfb648f00de79bb3783fe052ed8
isayeff <116463113+isayeff@users.noreply.github.com>
2024-01-01 17:09:29 +0400

    filter for continets update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,17 +6,16 @@ import { nanoid } from 'nanoid';
 function Card({count}) {
     const { data, setData } = useContext(DataCntx)
     const { cont } = useParams()
-    console.log(cont);
+    // console.log(cont);
 
     return (
         <>
-            {data.slice(0, count).map(item => (cont ? item.region.toLowerCase() == cont : item) && (
+            {data.slice(0, count).map(item => (
                 <article key={nanoid()} className="w-[292px] countryCards flex flex-col dark:bg-gray-900 shadow-md rounded-sm overflow-hidden">
                     <Link to={`/details/${item.cca3}`}>
                         <img alt="" className="object-cover w-full h-52 dark:bg-gray-500" src={item.flags.svg} />
                     </Link>
                     <div className="flex flex-col flex-1 p-6">
-                        {/* <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a> */}
                         <Link className="text-xs tracki uppercase hover:underline dark:text-violet-400">{item.region}</Link>
                         <h3 className="flex-1 py-2 text-lg font-semibold leadi text-white">{item.name.common}, <span className='italic'>{item.capital}</span></h3>
                         <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,24 +2,31 @@ import React, { useContext, useState } from 'react'
 import { DataCntx } from '../../data/DataContex'
 import Card from '../components/Card';
 import { useParams } from 'react-router-dom';
+import ContCards from '../components/ContCards';
 
 function Home() {
     const { data, setData } = useContext(DataCntx)
     const [load, setLoad] = useState(20)
-    console.log(data);
+    // console.log(data);
 
     function showMore() {
         setLoad(load + 40)
     }
+    const { cont } = useParams()
+    console.log(cont);
 
     return (
         <>
             <main className='bg-gray-800  py-10 min-h-[100vh]'>
                 <div className="cards flex flex-wrap gap-5 justify-center">
-                    <Card count = {load} />
+                    {
+                        cont == undefined ? 
+                        <Card count={load} />
+                        :<ContCards />
+                    }
                 </div>
                 <div className="loadBtn text-center">
-                    {load < data.length && <button className='bg-slate-900 text-white py-[10px] px-[20px] rounded-[5px] mt-[20px]' onClick={showMore}>Show More...</button>}
+                    {load < data.length && cont == undefined && <button className='bg-slate-900 text-white py-[10px] px-[20px] rounded-[5px] mt-[20px]' onClick={showMore}>Show More...</button>}
                 </div>
             </main>
         </>
