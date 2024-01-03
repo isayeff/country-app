a481d41eec53a4c27519fbdc5eded396024eb8e7
isayeff <116463113+isayeff@users.noreply.github.com>
2024-01-04 02:13:40 +0400

    Burger Menu added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import { DataCntx } from '../../data/DataContex';
 import { Link, useParams } from 'react-router-dom';
 import { nanoid } from 'nanoid';
 
-function Card({count}) {
+function Card({ count }) {
     const { data, setData } = useContext(DataCntx)
     const { cont } = useParams()
     // console.log(cont);
@@ -14,15 +14,15 @@ function Card({count}) {
                 <article key={nanoid()} className="w-[292px] countryCards flex flex-col dark:bg-gray-900 shadow-md rounded-sm overflow-hidden">
                     <Link to={`/details/${item.cca3}`}>
                         <img alt="" className="object-cover w-full h-52 dark:bg-gray-500" src={item.flags.svg} />
-                    </Link>
-                    <div className="flex flex-col flex-1 p-6">
-                        <Link className="text-xs tracki uppercase hover:underline dark:text-violet-400">{item.region}</Link>
-                        <h3 className="flex-1 py-2 text-lg font-semibold leadi text-white">{item.name.common}, <span className='italic'>{item.capital}</span></h3>
-                        <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
-                            <span>Population: {item.population}</span>
-                            <span>{item.area} km2</span>
+                        <div className="flex flex-col flex-1 p-6">
+                            <p className="text-xs tracki uppercase dark:text-violet-400">{item.region}</p>
+                            <h3 className="flex-1 py-2 text-lg font-semibold leadi text-white">{item.name.common}, <span className='italic'>{item.capital}</span></h3>
+                            <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
+                                <span>Population: {item.population}</span>
+                                <span>{item.area} km2</span>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                 </article>
             ))}
         </>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,13 +4,17 @@ import { IoEarthOutline, IoEarth } from "react-icons/io5";
 
 
 function Nav() {
-  const [earth, setEarth] = useState(true)
-  const interval = setInterval(() => {
-    setEarth(!earth)
-  }, 1000)
+  const [swtch, setSwtch] = useState(false)
+  // const [earth, setEarth] = useState(true)
+  // const interval = setInterval(() => {
+    // setEarth(!earth)
+  // }, 1000)
   // console.log(earth);
-
   const navigate = useNavigate()
+
+  function handleBurger() {
+    setSwtch(!swtch)
+  }
 
   return (
     <header className="p-4 dark:bg-gray-900 dark:text-zinc-300">
@@ -44,11 +48,28 @@ function Nav() {
             <NavLink to='/countries/antarctic' className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Antarctic</NavLink>
           </li>
         </ul>
-        <button title="Button" type="button" className="p-4 md:hidden">
+        <button onClick={handleBurger} title="Button" type="button" className="p-4 md:hidden">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 dark:text-gray-100">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
           </svg>
         </button>
+      </div>
+      <div className={`panel md:hidden ${swtch ? 'panelOpen' : ''}`}>
+        <ul className="flex flex-col items-center gap-[15px] py-[10px]">
+          <hr className='bg-white h-[1px] w-[50vw]' />
+          <li><NavLink to='/countries/europe'>Europe</NavLink></li>
+          <hr className='bg-white h-[1px] w-[50vw]' />
+          <li><NavLink to='/countries/asia'>Asia</NavLink></li>
+          <hr className='bg-white h-[1px] w-[50vw]' />
+          <li><NavLink to='/countries/americas'>Americas</NavLink></li>
+          <hr className='bg-white h-[1px] w-[50vw]' />
+          <li><NavLink to='/countries/africa'>Africa</NavLink></li>
+          <hr className='bg-white h-[1px] w-[50vw]' />
+          <li><NavLink to='/countries/oceania'>Oceania</NavLink></li>
+          <hr className='bg-white h-[1px] w-[50vw]' />
+          <li><NavLink to='/countries/antarctic'>Antarctic</NavLink></li>
+          <hr className='bg-white h-[1px] w-[50vw]' />
+        </ul>
       </div>
     </header>
   )
