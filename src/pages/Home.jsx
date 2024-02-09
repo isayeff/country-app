210e8b295c81ec0a03ead482877f6816585684b1
isayeff <116463113+isayeff@users.noreply.github.com>
2024-02-09 16:31:52 +0400

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,7 +39,7 @@ function Home() {
             <main className='bg-gray-800  py-10 min-h-[100vh] scroll-smooth'>
                 {
                     cont == undefined ?
-                        <div className="title flex flex-col justify-center items-center gap-[40px] h-[50vh] text-center">
+                        <div className="title flex flex-col justify-center items-center gap-[40px] h-[65vh] text-center">
                             <h2 className="inline text-3xl font-bold tracking-tight text-white sm:block sm:text-5xl">Welcome to CountryApp Website</h2>
                             <p className="inline text-3xl font-bold tracking-tight text-[#A78BFA] sm:block sm:text-4xl">You can find data of any country in this website.</p>
                             <Input getVal={getVal} handleSearch={handleSearch} state={state} />
